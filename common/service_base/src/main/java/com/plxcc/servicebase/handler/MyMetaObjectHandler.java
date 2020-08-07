a906686a5912724ec8fdd10d3c22a095eed254bc
plxc3 <2810323249@qq.com>
2020-08-07 19:55:27 +0800

    项目正式启动（项目建立）

--- common/service_base/src/main/java/com/plxcc/servicebase/handler/MyMetaObjectHandler.java
+++ common/service_base/src/main/java/com/plxcc/servicebase/handler/MyMetaObjectHandler.java
@@ -19,8 +19,8 @@ import java.util.Date;
 public class MyMetaObjectHandler implements MetaObjectHandler {
     @Override
     public void insertFill(MetaObject metaObject) {
-        this.setFieldValByName("gmtCreate",new Date(),metaObject);
-        this.setFieldValByName("gmtModified",new Date(),metaObject);
+        this.setFieldValByName("createTime",new Date(),metaObject);
+        this.setFieldValByName("modifiedTime",new Date(),metaObject);
     }
 
     @Override
