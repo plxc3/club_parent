34d3e49c4197e0ac564d52a806d43630ed1b79ea
plxc3 <2810323249@qq.com>
2020-08-09 00:56:37 +0800

    登陆注册功能全部完成

---
+++ gateway_api/src/main/java/com/plxcc/api/MainApplication.java
@@ -0,0 +1,20 @@
+package com.plxcc.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @PackgeName: com.plxcc.api
+ * @ClassName: MainApplication
+ * @Author: plxc
+ * Date: 2020/8/9 0:53
+ * project name: club_parent
+ * @Version:
+ * @Description:
+ */
+@SpringBootApplication
+public class MainApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MainApplication.class,args);
+    }
+}
--- hmy_project/service_gateway/src/main/java/com/hmy/gateway/GatewayMain.java
+++ hmy_project/service_gateway/src/main/java/com/hmy/gateway/GatewayMain.java
@@ -2,13 +2,10 @@ package com.hmy.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+@SpringBootApplication
 @EnableDiscoveryClient
-@ComponentScan({"com.hmy"})
 public class GatewayMain
 {
     public static void main(String[] args)
