a03a1c15dc9ed3a9ce7d3dc4f6f9fe8b878e07ed
plxc3 <2810323249@qq.com>
2020-08-09 00:43:50 +0800

    登陆注册功能全部完成

--- hmy_project/service_gateway/src/main/java/com/hmy/gateway/GatewayMain.java
+++ hmy_project/service_gateway/src/main/java/com/hmy/gateway/GatewayMain.java
@@ -2,10 +2,11 @@ package com.hmy.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 @EnableDiscoveryClient
 @ComponentScan({"com.hmy"})
 public class GatewayMain
